ae0a30d969709c1ccaa2ad972986a3d456807ad2
Matteo Emili <matteo.emili@live.com>
2015-04-29 20:15:15 +0100

    First commit for InstallSonarQube and InstallSonarRunner

---
+++ InstallSonarQube.ps1
@@ -0,0 +1,74 @@
+﻿# InstallSonarQube.ps1 - script for automatically installing SonarQube 5.1
+# It automates the scenario described in the SonarQube Installation Guide for Existing TFS Environment by the Visual Studio ALM Rangers
+# https://vsarguidance.codeplex.com/downloads/get/1452516
+#
+# matteo.emili@live.com || http://mattvsts.blogspot.com || @MattVSTS 
+# This script works on my machine and on my lab. I decline every responsibilities for problems you might have!
+
+param (
+$DatabaseServer = "DB",
+$NamedInstance = "INSTANCE",
+$DatabaseName = "SONAR",
+$User = "SAMPLE",
+$Password = "SAMPLE",
+$Port = "9090",
+$BinaryStore = "C:\SonarQube"
+)
+
+New-Item -Force -ItemType Directory -Path $BinaryStore
+
+# Paths for SonarQube 5.1, C# Plugin 4.0
+$SonarDLPath = "http://dist.sonar.codehaus.org/sonarqube-5.1.zip"
+$CsPluginDLPath = "http://repository.codehaus.org/org/codehaus/sonar-plugins/dotnet/csharp/sonar-csharp-plugin/4.0/sonar-csharp-plugin-4.0.jar"
+
+# Download SonarQube 5.1
+$SonarZIP = "$BinaryStore\sonarqube-5.1.zip"
+Invoke-WebRequest $SonarDLPath -OutFile $SonarZIP
+
+# Unblock the downloaded file
+Get-ChildItem $BinaryStore -Recurse | Unblock-File
+
+# Extract SonarQube
+Add-Type -Assembly "System.IO.Compression.FileSystem"
+[IO.Compression.ZipFile]::ExtractToDirectory($SonarZIP, $BinaryStore)
+
+# Delete the zip file
+Remove-Item $SonarZIP
+
+# Change User in sonar.properties
+$SonarProperties = "$BinaryStore\sonarqube-5.1\conf\sonar.properties"
+(Get-Content $SonarProperties) | Foreach-Object {
+    $_ -replace '^#sonar.jdbc.username=sonar$', ("sonar.jdbc.username="+$User)
+} | Set-Content $SonarProperties
+
+# Change Password in sonar.properties
+$SonarProperties = "$BinaryStore\sonarqube-5.1\conf\sonar.properties"
+(Get-Content $SonarProperties) | Foreach-Object {
+    $_ -replace '^#sonar.jdbc.password=sonar$', ("sonar.jdbc.password="+$Password)
+} | Set-Content $SonarProperties
+
+# Change Database in sonar.properties
+$SonarProperties = "$BinaryStore\sonarqube-5.1\conf\sonar.properties"
+if (![string]::IsNullOrEmpty($NamedInstance)) {
+    $DatabaseInstance = "$DatabaseServer\$NamedInstance"
+} 
+else {
+    $DatabaseInstance = $Database
+}
+
+(Get-Content $SonarProperties) | Foreach-Object {
+    $_ -replace '^#sonar.jdbc.url=jdbc:jtds:sqlserver://localhost/sonar;SelectMethod=Cursor$', ("sonar.jdbc.url=jdbc:jtds:sqlserver://$DatabaseInstance/$DatabaseName;SelectMethod=Cursor")
+} | Set-Content $SonarProperties
+
+# Change Port in sonar.properties
+$SonarProperties = "$BinaryStore\sonarqube-5.1\conf\sonar.properties"
+(Get-Content $SonarProperties) | Foreach-Object {
+    $_ -replace '^#sonar.web.port=9000$', ("sonar.web.port="+$Port)
+} | Set-Content $SonarProperties
+
+# Download the Sonar C# Plugin to the extensions\plugin folder
+$CsPluginJAR = "$BinaryStore\sonarqube-5.1\extensions\plugins\sonar-csharp-plugin-4.0.jar"
+Invoke-WebRequest $CsPluginDLPath -OutFile $CsPluginJAR
+
+# Run SonarQube
+Start-Process -FilePath $BinaryStore\sonarqube-5.1\bin\windows-x86-64\StartSonar.bat -Wait
---
+++ InstallSonarRunner.ps1
@@ -0,0 +1,84 @@
+﻿# InstallSonarQube.ps1 - script for automatically installing SonarQube Runner 2.4 and Sonar MSBuild Runner 0.9
+# It automates the scenario described in the SonarQube Installation Guide for Existing TFS Environment by the Visual Studio ALM Rangers
+# https://vsarguidance.codeplex.com/downloads/get/1452516
+#
+# matteo.emili@live.com || http://mattvsts.blogspot.com || @MattVSTS 
+# This script works on my machine and on my lab. I decline every responsibilities for problems you might have!
+
+param (
+$BinaryStore = "C:\SonarQube",
+$SonarServer = "SAMPLE",
+$SonarPort = 9090,
+[bool]$MSBuildv12 = $true,
+[bool]$MSBuildv14 = $false
+)
+
+New-Item -Force -ItemType Directory -Path $BinaryStore
+
+$RunnerDLPath = "http://repo1.maven.org/maven2/org/codehaus/sonar/runner/sonar-runner-dist/2.4/sonar-runner-dist-2.4.zip"
+$MSBuildRunnerDLPath = "http://dist.sonarsource.com/csharp/download/SonarQube.MSBuild.Runner-0.9.zip"
+
+$RunnerZIP = "$BinaryStore\sonar-runner-dist-2.4.zip"
+$MSBuildRunnerZIP = "$BinaryStore\SonarQube.MSBuild.Runner-0.9.zip"
+
+Invoke-WebRequest $RunnerDLPath -OutFile $RunnerZIP
+
+# Unblock the downloaded file
+Get-ChildItem $BinaryStore -Recurse | Unblock-File
+
+# Extract Sonar Runner
+Add-Type -Assembly "System.IO.Compression.FileSystem"
+[IO.Compression.ZipFile]::ExtractToDirectory($RunnerZIP, $BinaryStore)
+
+# Change URL in sonar-runner.properties
+$SonarRunnerProperties = "$BinaryStore\sonar-runner-2.4\conf\sonar-runner.properties"
+(Get-Content $SonarRunnerProperties) | Foreach-Object {
+    $_ -replace '^#sonar.host.url=http://localhost:9000$', ("sonar.host.url=http://$SonarServer"+":$SonarPort")
+} | Set-Content $SonarRunnerProperties
+
+# Change the PATH Environment Variable by adding the Sonar Runner bin path
+$path = ";$BinaryStore\sonar-runner-2.4\bin"
+[Environment]::SetEnvironmentVariable("PATH", $env:PATH+$path, "Machine" )
+
+# Create the SONAR_RUNNER_HOME Environment Variable
+[Environment]::SetEnvironmentVariable("SONAR_RUNNER_HOME", "$BinaryStore\sonar-runner-2.4", "Machine")
+
+# Create the SONAR_RUNNER_OPTS Environment Variable
+[Environment]::SetEnvironmentVariable("SONAR_RUNNER_OPTS", "-Xmx512m -XX:MaxPermSize=128m", "Machine")
+
+# Download the MSBuild Sonar Runner
+Invoke-WebRequest $MSBuildRunnerDLPath -OutFile $MSBuildRunnerZIP
+
+# Extract Sonar Runner
+Add-Type -Assembly "System.IO.Compression.FileSystem"
+[IO.Compression.ZipFile]::ExtractToDirectory($MSBuildRunnerZIP, "$BinaryStore\bin")
+
+# Copy the TARGETS file to the relevant MSBuild version
+
+if ($MSBuildv12){
+    $PathMSBv12 = ${env:ProgramFiles(x86)}+ "\MSBuild\12.0\Microsoft.Common.Targets\ImportBefore"
+      
+    if(!(Test-Path -Path $PathMSBv12)){
+        New-Item -ItemType directory -Path $PathMSBv12
+    }
+    Copy-Item $BinaryStore\bin\SonarQube.Integration.ImportBefore.targets $PathMSBv12
+    Restart-Service TFSBuildServiceHost.2013
+}
+
+if ($MSBuildv14){
+    $PathMSBv14 = ${env:ProgramFiles(x86)}+ "\MSBuild\14.0\Microsoft.Common.Targets\ImportBefore"
+      
+    if(!(Test-Path -Path $PathMSBv14)){
+        New-Item -ItemType directory -Path $PathMSBv14
+    }
+    Copy-Item $BinaryStore\bin\SonarQube.Integration.ImportBefore.targets $PathMSBv14
+    Restart-Service TFSBuildServiceHost.2013
+}
+
+
+
+
+
+
+
+
