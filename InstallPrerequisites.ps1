0f00ebd0462c81e2c056cb54428dc5ffb0a01e1e
Matteo Emili <matteo.emili@live.com>
2015-04-29 21:14:56 +0100

    JRE unattended installation

---
+++ InstallPrerequisites.ps1
@@ -0,0 +1,12 @@
+﻿
+# Download and install the appropriate Java Runtime Environment for your OS
+$OSVersion = Get-WmiObject -ComputerName localhost Win32_OperatingSystem | select OSArchitecture
+if ($OSVersion = "@{OSArchitecture=64-bit}"){
+    $JRE = "$env:USERPROFILE\Downloads\JRE64.exe"
+    Invoke-WebRequest http://javadl.sun.com/webapps/download/AutoDL?BundleId=106248 -OutFile $JRE
+} else {
+    $JRE = "$env:USERPROFILE\Downloads\JRE32.exe"
+    Invoke-WebRequest http://javadl.sun.com/webapps/download/AutoDL?BundleId=106246 -OutFile $JRE
+}
+Start-Process -FilePath $JRE -ArgumentList "/s" -PassThru -Wait
+
