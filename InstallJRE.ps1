4016839266d2c2a3632bacffd8d9424e0b827804
matteo.emili@live.com <matteo.emili@live.com>
2015-04-30 10:11:36 +0100

    Renamed the script to InstallJRE for better componentization.

--- InstallJRE.ps1
+++ InstallJRE.ps1
@@ -1,4 +1,10 @@
-﻿
+﻿# InstallJRE - script for automatically installing the Java Runtime Environment
+# It automates the scenario described in the SonarQube Installation Guide for Existing TFS Environment by the Visual Studio ALM Rangers
+# https://vsarguidance.codeplex.com/downloads/get/1452516
+#
+# matteo.emili@live.com || http://mattvsts.blogspot.com || @MattVSTS 
+# This script works on my machine and on my lab. I decline every responsibilities for problems you might have!
+
 # Download and install the appropriate Java Runtime Environment for your OS
 $OSVersion = Get-WmiObject -ComputerName localhost Win32_OperatingSystem | select OSArchitecture
 if ($OSVersion = "@{OSArchitecture=64-bit}"){
